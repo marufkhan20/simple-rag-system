7e5cf8e0896fe908f7b0ba22fb9b2cc49a96737b
marufkhan20 <mdm326040@gmail.com>
2026-08-14 23:32:41 +0600

    implement split documents function in ingestion_pipeline for the chunk documents

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -36,12 +36,40 @@ def load_documents(docs_path="docs"):
 
     return documents
 
+def split_documents(documents, chunk_size=1000, chunk_overlap=0):
+    # Split documents into smaller chunks with overlap
+    print(f"Splititng documents into chunks...")
+
+    text_splitter = CharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    if chunks:
+        for i, chunk in enumerate(chunks[:5]): # show first 5 chunk
+            print(f"\n--- Chunk {i+1} ---")
+            print(f"Source: {chunk.metadata['source']}")
+            print(f"Length: {len(chunk.page_content)} characters")
+            print(f"Content:")
+            print(chunk.page_content)
+            print("-" * 50)
+        
+        if len(chunks) > 5:
+            print(f"\n... and {len(chunks) - 5} more chunks")
+    
+    return chunks
+
+
 def main():
     print("Main function")
 
     # 1. Loading files
     documents = load_documents(docs_path="docs")
-    
+
+    # 2. Chunking the files
+    chunks = split_documents(documents)
 
 if __name__ == "__main__":
     main()
